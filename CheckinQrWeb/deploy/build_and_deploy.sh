191e2f6e31dbbf4ea9f0e9e2e21f6bddb7fced70
playtime222 <steve.kellaway@mefitihe.com>
2022-05-05 21:16:29 +0200

    Fixed environment issue (again!)

--- CheckinQrWeb/deploy/build_and_deploy.sh
+++ CheckinQrWeb/deploy/build_and_deploy.sh
@@ -1,4 +1,4 @@
-docker run -p 7002:80/tcp -p --name wallet wallet:pub_latest
+docker run -p 7002:80/tcp -p wallet wallet:pub_latest
 #docker run -p 7002:80/tcp -p --name wallet wallet:pub_latest 
 #docker run -p 80:80/tcp -p 443:443/tcp --name wallet wallet:pub_latest
 #docker run -p 8082:8082/tcp --name CheckInQrWeb checkinqrweb:latest
